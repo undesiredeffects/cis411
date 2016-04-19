e405888d87b08cf489cd523e0490061fe3981699
Christopher Jones <cejone08@louisville.edu>
2016-04-19 16:36:24 -0400

    initial commit

---
+++ WeddingPage/WeddingPage/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingPage
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
